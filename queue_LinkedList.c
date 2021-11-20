b0dda3aef61f5e5e082851b6cd140ddc535472ba
Drako <90750669+coffee-sugar@users.noreply.github.com>
2021-11-20 10:47:08 +0530

    formatted

--- queue_LinkedList.c
+++ queue_LinkedList.c
@@ -1,53 +1,64 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-struct node * f, * r;
+struct node *f, *r;
 
-struct node{
+struct node
+{
     int data;
-    struct node * next;
+    struct node *next;
 };
 
-int isFull(){
-    
-    return 0 ;
+int isFull()
+{
+
+    return 0;
 }
 
-void LLtraversal(struct node * ptr){
-    while (ptr!=NULL)
+void LLtraversal(struct node *ptr)
+{
+    while (ptr != NULL)
     {
-        printf("\nThe element is: %d",ptr->data);   
-        ptr=ptr->next;
+        printf("\nThe element is: %d", ptr->data);
+        ptr = ptr->next;
     }
 }
 
-void enqueue( int val){
-    struct node * n= (struct node * ) malloc (sizeof(struct node));
-    if(n==NULL){
+void enqueue(int val)
+{
+    struct node *n = (struct node *)malloc(sizeof(struct node));
+    if (n == NULL)
+    {
         printf("\nQueue is FUll...");
     }
-    else {
-        n->data= val;
+    else
+    {
+        n->data = val;
         n->next = NULL;
-        if(f==NULL){
-            f=r=n;
+        if (f == NULL)
+        {
+            f = r = n;
         }
-        else{
-            r->next=n;
-            r=n;
+        else
+        {
+            r->next = n;
+            r = n;
         }
     }
 }
 
-int dequeue(){
+int dequeue()
+{
     int val = -1;
-    struct node * ptr=f;
-    if(f==NULL){
+    struct node *ptr = f;
+    if (f == NULL)
+    {
         printf("\nThe queue is Empty...");
     }
-    else{
-        val= ptr -> data;
-        f=f->next;
+    else
+    {
+        val = ptr->data;
+        f = f->next;
         free(ptr);
     }
     return val;
@@ -60,7 +71,7 @@ int main()
     enqueue(35);
     enqueue(3);
     LLtraversal(f);
-    printf("\nThe dequeue: %d",dequeue());
+    printf("\nThe dequeue: %d", dequeue());
     LLtraversal(f);
     return 0;
 }
